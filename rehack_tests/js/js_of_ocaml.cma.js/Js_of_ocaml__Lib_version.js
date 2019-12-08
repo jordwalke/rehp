9118dff79114dc566fcd34cc447865c7d2464877
Jordan Walke <jordojw@gmail.com>
2019-12-08 14:23:58 -0800

    Remove no longer used raw_backend stubs

--- rehack_tests/js/js_of_ocaml.cma.js/Js_of_ocaml__Lib_version.js
+++ rehack_tests/js/js_of_ocaml.cma.js/Js_of_ocaml__Lib_version.js
@@ -13,7 +13,7 @@ let joo_global_object = global;
 var runtime = joo_global_object.jsoo_runtime;
 var string = runtime["caml_new_string"];
 var s = string("3.3.0");
-var git_version = string("79639f9c6");
+var git_version = string("5e9257f77");
 var Js_of_ocaml_Lib_version = [0,s,git_version];
 
 runtime["caml_register_global"](
--- runtime/mlString.js
+++ runtime/mlString.js
@@ -446,7 +446,7 @@ function caml_jsbytes_of_string (s) {
 }
 
 //Provides: caml_js_to_string const
-//Requires: caml_is_ascii, caml_utf8_of_utf16, MlBytes, Error
+//Requires: caml_is_ascii, caml_utf8_of_utf16, MlBytes
 function caml_js_to_string (s) {
   if(typeof s !== "string") {
     throw new Error("caml_js_to_string called with non-string");
@@ -456,30 +456,6 @@ function caml_js_to_string (s) {
     tag = 8 /* BYTES | NOT_ASCII */, s = caml_utf8_of_utf16(s);
   return new MlBytes(tag, s, s.length);
 }
-
-// The compiler can emit calls to caml_js_to_string("php-string") but
-// converting stubs from JS->php can result in
-// caml_js_to_string($String->new("str")).
-//Provides: caml_js_to_string const
-//Requires: caml_is_ascii, caml_utf8_of_utf16, MlBytes, Error, Func, String
-//ForBackend: php
-var caml_js_to_string = raw_backend([
-  "$caml_js_to_string = $Func(",
-  "  function($s) use ($Error,$MlBytes,$String,$caml_is_ascii,$eqEqEq,$typeof) {",
-  "    if (PHP\is_string($s)) {",
-  "      $s=$String->new($s);",
-  "    }",
-  "    if (!$eqEqEq($typeof($s), $String->new(\"string\"))) {",
-  "      throw $Error->new(",
-  "              $String->new(\"caml_js_to_string called with non-string\")",
-  "            );",
-  "    }",
-  "    $tag = 9;",
-  "    if (! $caml_is_ascii($s)) {($tag = 8) || true ? $s = $s : ($s = $s);}",
-  "    return $MlBytes->new($tag, $s, $s->length);",
-  "  }",
-  ");",
-]);
 
 //Provides: caml_create_string const
 //Requires: MlBytes,caml_invalid_argument
@@ -497,21 +473,6 @@ function caml_create_bytes(len) {
 //Provides: caml_new_string const (const)
 //Requires: MlBytes
 function caml_new_string (s) { return new MlBytes(0,s,s.length); }
-
-//Provides: caml_new_string_impl const (const)
-//Requires: MlBytes
-//ForBackend: php
-function caml_new_string_impl (s) { return new MlBytes(0,s,s.length); }
-
-//Provides: caml_new_string const (const)
-//Requires: MlBytes, caml_new_string_impl
-//ForBackend: php
-function caml_new_string (s) {
-  if(!(s instanceof String)) {
-    return caml_new_string_impl(new String(s));
-  }
-  return caml_new_string_impl(s);
-}
 
 //Provides: caml_string_of_array
 //Requires: MlBytes
